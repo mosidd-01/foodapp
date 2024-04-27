8a39ca8f8b7f0aa11838fbda2e7167dc2db2fafc
Muhammad Siddiqui <s760132@gmail.com>
2024-04-27 01:52:37 -0500

    Added Images of food along with a button to view recipe

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
 import Search from "./components/Search";
+import FoodList from "./components/FoodList";
 
 function App() {
+  const [foodData, setFoodData] = useState([]);
+  if (foodData.length == 0) {
+    setFoodData([1]);
+    //console.log("asdas");
+  }
   return (
     <div className="App">
-      <Search />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <FoodList foodData={foodData} setFoodData={setFoodData} />
+      {/* {foodData.title} */}
+      {/* {foodData.map((food) => (
+        <h1>{food.title}</h1>
+      ))} */}
     </div>
   );
 }
--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -3,14 +3,22 @@ import { useEffect, useState } from "react";
 const URL = import.meta.env.VITE_API_URL;
 const API = import.meta.env.VITE_API_KEY;
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
   //syntax of the useEffect hook
   useEffect(() => {
     async function fetchFood() {
-      const res = await fetch(`${URL}?query=${query}&apiKey=${API}`);
-      const data = await res.json();
-      console.log(data.results);
+      try {
+        const res = await fetch(`${URL}?query=${query}&apiKey=${API}`);
+        const data = await res.json();
+        //const data = await res;
+        console.log(typeof data.results);
+        console.log(data.results);
+        console.log(setFoodData);
+        setFoodData(data.results);
+      } catch (error) {
+        console.log(error);
+      }
     }
     fetchFood();
   }, [query]);
